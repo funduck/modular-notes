cf1dd21be3587a99539ef86c273e46ca7388fb02
funduck <qlfunduck@gmail.com>
2019-03-09 12:20:23 +0300

    more talkative output

--- bin/description.sh
+++ bin/description.sh
@@ -17,27 +17,37 @@ case "$command" in
   update|check)
     case "$module" in
       all)
-        ls $root/api/* | grep -E "^[a-z]*(.json|.yaml|.yml)" | head -1 | while read f ;
+        echo "compiling openapi -> description for all modules"
+        ls $root/api/* | grep -E "/[a-z]*(.json|.yaml|.yml)" | while read f ;
         do
-          filename=`echo $filename | sed 's/\(.*\)\.xls/\1/'`
-          module=`echo $f | sed 's/\(.*\)\.*/\1/'`
-          [ -d $root/$module ] && node $root/utils/nodejs/openapi2description.js $root/api/$f > $root/$module/description.json
+          module=`echo $f | sed 's/.*\/\(.*\)\..*/\1/'`
+          [ -d $root/$module ] && { \
+            echo compiling $f
+            node $root/utils/nodejs/openapi2description.js $f > $root/$module/description.json
+          } || echo "module '$module' not exists"
         done
       ;;
       *)
+        echo "compiling openapi -> description for '$module'"
         ls $root/api/$module.* | grep -E "(.json|.yaml|.yml)" | head -1 | while read f ;
         do
           tmp="$root/$module/description.json"
-          [ $command = check ] && tmp="/tmp/modular-notes-description.json"
-          [ -d $root/$module ] && node $root/utils/nodejs/openapi2description.js $f > $tmp
-          [ $command = check ] && [ -e $tmp ] && { diff $tmp $root/$module/description.json ; rm $tmp ; }
+          [ $command = check ] && tmp="/tmp/modular-notes-description.json" && [ -e $tmp ] && rm $tmp
+          [ -d $root/$module ] && { \
+            echo "compiling $f into $tmp"
+            node $root/utils/nodejs/openapi2description.js $f > $tmp
+          }
+          [ $command = check ] && [ -e $tmp ] && { \
+            echo "checking diff"
+            diff $tmp $root/$module/description.json
+          }
         done 
       ;;
     esac
   ;;
   *)
-    echo "Usage: sh description.sh COMMAND MODULE"
-    echo "commands: check, update"
+    echo "Usage: sh description.sh MODULE COMMAND"
     echo "modules: storage, server, ..., all"
+    echo "commands: check, update"
   ;;
 esac
